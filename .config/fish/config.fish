33239c8c3c34449aea93cdb2e38b7b09f37aa639
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2023-08-11 15:05:32 +0900

    fish: Disable welcome message

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-export PATH="$HOME/Library/Python/3.9/lib/python/site-packages:$HOME/Library/Python/3.9/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin:/usr/local/bin:$PATH"
+    export PATH="$HOME/Library/Python/3.9/lib/python/site-packages:$HOME/Library/Python/3.9/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin:/usr/local/bin:$PATH"
 
-alias gcc='gcc-13'
-alias g++='g++-13'
+    alias gcc='gcc-13'
+    alias g++='g++-13'
+
+    set -U fish_greeting
 
 end
