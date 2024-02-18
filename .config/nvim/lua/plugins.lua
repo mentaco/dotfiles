eb6b84737c3dde589ca0e167da126da105b4a8f0
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2024-02-18 23:47:50 +0900

    nvim: Add nvim-treesitter

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,10 +1,23 @@
 return {
     {
-        'Mofiqul/dracula.nvim',
+        "Mofiqul/dracula.nvim",
         config = true,
     },
     {
         "nvim-telescope/telescope-file-browser.nvim",
         dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function ()
+            local configs = require("nvim-treesitter.configs")
+
+            configs.setup({
+                ensure_installed = { "bash", "c", "cpp", "dart", "lua", "vim", "python" },
+                highlight = { enable = true },
+                indnet = { enable = true },
+            })
+        end
     },
 }
