0bd6c7b5092c0afd72f4db45f48119e4efbca1a1
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2024-03-11 15:40:42 +0900

    nvim: Change bufferline style

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -2,5 +2,24 @@ return {
     'akinsho/bufferline.nvim',
     version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons',
-    config = true,
+    config = function()
+        local bufferline = require('bufferline')
+        bufferline.setup({
+            options = {
+                indicator = {
+                    style = "underline",
+                },
+                offsets = {
+                    {
+                        filetype = "NvimTree",
+                        text = function()
+                          return vim.fn.getcwd()
+                        end,
+                        highlight = "Directory",
+                    }
+                },
+
+            },
+        })
+    end
 }
