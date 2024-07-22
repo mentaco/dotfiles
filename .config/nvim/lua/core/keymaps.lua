5d6f3511ceafd5bdd6bb0c3092544d83be94367b
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2024-07-22 23:07:16 +0900

    nvim: Rewrite "vim.api.nvim_set_keymap" to "vim.keymap.set"

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,28 +1,28 @@
 -- buffer control
-vim.keymap.set('n', '<C-J>', '<Cmd>bprevious<CR>', { noremap = true })
-vim.keymap.set('n', '<C-K>', '<Cmd>bnext<CR>', { noremap = true })
+vim.keymap.set('n', '<C-J>', '<Cmd>bprevious<CR>')
+vim.keymap.set('n', '<C-K>', '<Cmd>bnext<CR>')
 
 -- tabpage control
-vim.keymap.set('n', 'st', '<Cmd>tabnew<CR>', { noremap = true })
-vim.keymap.set('n', 'sn', '<Cmd>tabnext<CR>', { noremap = true })
-vim.keymap.set('n', 'sp', '<Cmd>tabprevious<CR>', { noremap = true })
-vim.keymap.set('n', 'sc', '<Cmd>tabclose<CR>', { noremap = true })
+vim.keymap.set('n', 'st', '<Cmd>tabnew<CR>')
+vim.keymap.set('n', 'sn', '<Cmd>tabnext<CR>')
+vim.keymap.set('n', 'sp', '<Cmd>tabprevious<CR>')
+vim.keymap.set('n', 'sc', '<Cmd>tabclose<CR>')
 
 -- window control
-vim.keymap.set('n', '<S-J>', '<Cmd>resize -2<CR>', { noremap = true })
-vim.keymap.set('n', '<S-K>', '<Cmd>resize +2<CR>', { noremap = true })
-vim.keymap.set('n', '<S-H>', '<Cmd>vertical resize -2<CR>', { noremap = true })
-vim.keymap.set('n', '<S-L>', '<Cmd>vertica resize +2<CR>', { noremap = true })
+vim.keymap.set('n', '<S-J>', '<Cmd>resize -2<CR>')
+vim.keymap.set('n', '<S-K>', '<Cmd>resize +2<CR>')
+vim.keymap.set('n', '<S-H>', '<Cmd>vertical resize -2<CR>')
+vim.keymap.set('n', '<S-L>', '<Cmd>vertica resize +2<CR>')
 
 -- cursor
-vim.keymap.set('n', '<C-H>', '<S-H>', { noremap = true })
-vim.keymap.set('n', '<C-L>', '<S-L>', { noremap = true })
+vim.keymap.set('n', '<C-H>', '<S-H>')
+vim.keymap.set('n', '<C-L>', '<S-L>')
 
 -- concatenation of rows
-vim.keymap.set('n', '<S-U>', '<S-J>', { noremap = true })
+vim.keymap.set('n', '<S-U>', '<S-J>')
 
 -- telescope file browser
-vim.api.nvim_set_keymap( "n", "<leader>ts", ":Telescope file_browser<CR>", { noremap = true })
+vim.keymap.set( 'n', '<leader>ts', ':Telescope file_browser<CR>')
 
 -- NvimTree
-vim.api.nvim_set_keymap( "n", "<C-N>", ":NvimTreeToggle<CR>", { noremap = true })
+vim.keymap.set('n', '<C-N>', ':NvimTreeToggle<CR>')
