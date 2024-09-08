cbcf2d3f21979d486d3f31ed0dc983ece2d8b25f
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2024-09-08 18:35:11 +0900

    nvim: Add formatters and linters

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -4,13 +4,14 @@ return {
 	dependencies = {
 		"williamboman/mason.nvim",
 		"nvimtools/none-ls.nvim",
+		"gbprod/none-ls-shellcheck.nvim",
 	},
 	config = function()
 		local mason_null_ls = require("mason-null-ls")
 		local null_ls = require("null-ls")
 
 		mason_null_ls.setup({
-			ensure_installed = { "stylua", "black", "isort" },
+			ensure_installed = { "stylua", "black", "isort", "clang-format", "shfmt", "shellcheck" },
 			automatic_installation = true,
 			handlers = {},
 		})
@@ -18,6 +19,10 @@ return {
 		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 		null_ls.setup({
 			-- debug = true,
+			sources = {
+				require("none-ls-shellcheck.diagnostics"),
+				require("none-ls-shellcheck.code_actions"),
+			},
 			on_attach = function(client, bufnr)
 				-- formatting on save
 				if client.supports_method("textDocument/formatting") then
