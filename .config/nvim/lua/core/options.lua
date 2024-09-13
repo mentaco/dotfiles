c556e14d05963eb909a4c3a032a76fbda7344824
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2024-09-13 11:11:43 +0900

    nvim: Set C/C++ indentation to 2

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,10 +1,10 @@
-vim.opt.encoding = 'utf-8'
+vim.opt.encoding = "utf-8"
 vim.opt.backup = false
 vim.opt.number = true
-vim.opt.virtualedit = 'onemore'
+vim.opt.virtualedit = "onemore"
 vim.opt.wildmenu = true
 vim.opt.hidden = true
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 vim.opt.termguicolors = true
 
 vim.opt.showmatch = true
@@ -25,21 +25,20 @@ vim.opt.autoindent = true
 vim.opt.smartindent = true
 
 -- filetype
-vim.cmd('autocmd FileType c setlocal cindent')
-vim.cmd('autocmd FileType cpp setlocal cindent')
-vim.cmd('autocmd FileType dart setlocal tabstop=2 shiftwidth=2 softtabstop=2')
+vim.cmd("autocmd FileType c setlocal cindent tabstop=2 shiftwidth=2 softtabstop=2")
+vim.cmd("autocmd FileType cpp setlocal cindent tabstop=2 shiftwidth=2 softtabstop=2")
+vim.cmd("autocmd FileType dart setlocal tabstop=2 shiftwidth=2 softtabstop=2")
 
 -- background
 vim.opt.termguicolors = true
 vim.opt.winblend = 0
 vim.opt.pumblend = 0
 
-
 -- Hide line numbers when flutter log window is opened
 vim.api.nvim_create_autocmd("BufWinEnter", {
-    pattern = "__FLUTTER_DEV_LOG__*",
-    callback = function()
-        vim.wo.number = false
-        vim.wo.relativenumber = false
-    end
+	pattern = "__FLUTTER_DEV_LOG__*",
+	callback = function()
+		vim.wo.number = false
+		vim.wo.relativenumber = false
+	end,
 })
