2b3047a7b579a8176cc73c314c96cd003aac020e
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2024-09-19 12:37:41 +0900

    fish: Setup flutter

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,6 @@ if status is-interactive
     # Disable welcome message
     set fish_greeting
 end
+
+# Added by `rbenv init` on Thu Sep 19 12:03:44 JST 2024
+status --is-interactive; and rbenv init - --no-rehash fish | source
