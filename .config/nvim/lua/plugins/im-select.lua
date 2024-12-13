e6717060756431776363d7b98c2b0d9e833be819
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2024-12-13 16:25:14 +0900

    nvim: Update im-select config

--- .config/nvim/lua/plugins/im-select.lua
+++ .config/nvim/lua/plugins/im-select.lua
@@ -1,8 +1,13 @@
-return {
-    "keaising/im-select.nvim",
-    config = function()
-        require("im_select").setup({
-            default_im_select = "com.apple.keylayout.ABC"
-        })
-    end,
-}
+local uname = vim.loop.os_uname()
+local is_mac = uname.sysname == "Darwin"
+
+if is_mac then
+    return {
+        "keaising/im-select.nvim",
+        config = function()
+            require("im_select").setup({
+                default_im_select = "com.apple.keylayout.ABC"
+            })
+        end,
+    }
+end
