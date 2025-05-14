504ebcc8389c2e90255c6e3cfa177f8d42aa67c1
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2025-05-14 14:12:31 +0900

    fish: Remove fish config

--- .config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    
-    # Disable welcome message
-    set fish_greeting
-end
-
-# Added by `rbenv init` on Thu Sep 19 12:03:44 JST 2024
-status --is-interactive; and rbenv init - --no-rehash fish | source
