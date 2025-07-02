2ac554b8f45e0673cb63c6db2bc73f8ec2c7c297
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2025-07-02 11:28:40 +0900

    install.sh: Update script

--- install.sh
+++ install.sh
@@ -5,20 +5,32 @@ set -ue
 cd "$(dirname "$0")"
 DOT_DIR="$(pwd)"
 DOT_CONF_DIR="$(pwd)/.config"
+EXCLUDES=(".git" ".gitconfig" ".gitignore" ".DS_Store")
 
-for f in ${DOT_DIR}/.??*; do
-    [[ "$(basename ${f})" == ".git" ]] && continue
-    [[ "$(basename ${f})" == ".gitconfig" ]] && continue
-    [[ "$(basename ${f})" == ".gitignore" ]] && continue
-    [[ "$(basename ${f})" == ".DS_Store" ]] && continue
-    [[ "$(basename ${f})" =~ .swp$ ]] && continue
-    
-    if [[ "$(basename ${f})" == ".config" ]]; then
-        for f in ${DOT_CONF_DIR}/*; do
-            command ln -snfv "${f}" $HOME/.config
-        done
-    else
-        command ln -snfv "${f}" $HOME
-    fi
+is_excluded() {
+	local name="$1"
+	for exclude in "${EXCLUDES[@]}"; do
+		[[ "$name" == "$exclude" ]] && return 0
+	done
+	[[ "$name" =~ \.swp$ ]] && return 0
+	return 1
+}
+
+link_file() {
+	local src="$1"
+	local dest="$2"
+	ln -snfv "$src" "$dest"
+}
+
+for f in "$DOT_DIR"/.??*; do
+	name="$(basename "$f")"
+	is_excluded "$name" && continue
+
+	if [[ "$name" == ".config" ]]; then
+		for cf in "$DOT_CONF_DIR"/*; do
+            link_file "$cf" "$HOME/.config"
+		done
+	else
+		link_file "$f" "$HOME"
+	fi
 done
-
