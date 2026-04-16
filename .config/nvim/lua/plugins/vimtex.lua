4cefa70723cd7f9a5fe315608a304a1cdf5c7885
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2026-04-16 23:24:17 +0900

    nvim: Add Linux fallback viewer for vimtex

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -3,6 +3,10 @@ return {
 	lazy = false, -- we don't want to lazy load VimTeX
 	-- tag = "v2.15", -- uncomment to pin to a specific release
 	init = function()
-		vim.g.vimtex_view_method = "skim"
+		if vim.fn.has("mac") == 1 then
+			vim.g.vimtex_view_method = "skim"
+		else
+			vim.g.vimtex_view_method = "zathura"
+		end
 	end,
 }
